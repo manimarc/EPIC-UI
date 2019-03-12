87a3d70c138eff3b8137f39bce9e49f0a8f0f5d4
manimarc <mambest001@gmail.com>
2019-03-12 14:50:55 +0100

    [FT] creation of ui with a dashboard page

---
+++ ui/js/epiclogin.js
@@ -0,0 +1,22 @@
+
+const uemail=document.getElementById('emailid');
+
+const upassword=document.getElementById('passwordid');
+
+const loginBtn=document.getElementById('profile_btn');
+const responseMessage  = document.getElementById('dmessage');
+
+
+
+loginBtn.addEventListener('click',(event)=>{
+const	upassword_v = upassword.value;
+const	uemail_v = uemail.value;
+	/*if(upassword_v  !== '')
+	{*/
+window.location.href = '../index.html';
+/*}else{
+	alert('All field should be filled!');
+}*/
+});
+
+
